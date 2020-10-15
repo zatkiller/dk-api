507ca4bb321d084c21b860ce6c88febf9eb62d35
Ivan Tan <ivantjh97@gmail.com>
2020-10-15 23:09:42 +0800

    Add dotenv

--- index.ts
+++ index.ts
@@ -1,13 +1,15 @@
 import express from 'express';
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || '3000';
 
 app.get('/', (req, res) => {
   res.send("Hello World!");
 })
 
-app.listen(3000, () => {
+app.listen(PORT, () => {
   console.log(`Express server is listening on ${PORT}`);
 })
-
